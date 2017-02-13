460c525716d24de833749e8d454e5b1c90f3abfa
Pedro Fernandes <prmf06@gmail.com>
2017-02-13 08:00:10 +0000

    Initial and Final version

---
+++ runme.bat
@@ -0,0 +1,7 @@
+DEL /F /S /Q /A "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*"
+
+REG DELETE HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband /F
+
+taskkill /f /im explorer.exe
+
+start explorer.exe
